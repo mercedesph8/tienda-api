b9184e2a0139a452748af62850854025683a55bc
Pepe <josejesusvelazquez.24@campuscamara.es>
2025-10-31 12:49:12 +0100

    1.4

---
+++ controllers/proveedoresController.js
@@ -0,0 +1,22 @@
+//Clase completa, hacer las demás
+const productosService = require('../services/proveedoresService');
+exports.obtenerTodos = (req, res) => {
+const proveedores = proveedoresService.listar();
+res.json(proveedores);
+};
+exports.obtenerPorId = (req, res) => {
+const proveedor = proveedoresService.buscarPorId(parseInt(req.params.id));
+proveedor ? res.json(proveedor) : res.status(404).json({ mensaje: 'No encontrado' });
+};
+exports.crear = (req, res) => {
+const nuevo = proveedoresService.crear(req.body);
+res.status(201).json(nuevo);
+};
+exports.actualizar = (req, res) => {
+const actualizado = proveedoresService.actualizar(parseInt(req.params.id), req.body);
+actualizado ? res.json(actualizado) : res.status(404).json({ mensaje: 'No encontrado' });
+};
+exports.eliminar = (req, res) => {
+const eliminado = proveedoresService.eliminar(parseInt(req.params.id));
+eliminado ? res.json(eliminado) : res.status(404).json({ mensaje: 'No encontrado' });
+};
